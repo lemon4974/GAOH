3c267e8e44ac20564045e57154bfa341e594cc9c
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-18 18:36:26 +0900

    💄 home 필름 css

---
+++ src/components/Main/MovieReel.jsx
@@ -0,0 +1,120 @@
+import React from 'react';
+
+import '../../styles/home/moviereel.scss';
+
+export default function MovieReel() {
+  return (
+    <div>
+      {/* <div class="strip">
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+      </div> */}
+
+      <div class="strip">
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+        <div class="film">
+          <div class="film__frame">
+            <img src="https://via.placeholder.com/1920x1080" />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
