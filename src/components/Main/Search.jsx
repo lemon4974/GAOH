b887560154a127aa2e1d775ee7c68075c100e9c4
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-19 18:13:08 +0900

    🚧 btn css

---
+++ src/components/Main/Search.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import '../../styles/home/search.scss';
+
+export default function Search() {
+  return (
+    <div>
+      Search
+      {/* <div className="box-grid-item">
+        <div className="box six">hihi</div>
+      </div> */}
+      <button>Offset Button</button>
+    </div>
+  );
+}
