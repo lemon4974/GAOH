2017945094bea55ef4ea0c48a6a6b6fb540bcfe3
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-20 17:41:26 +0900

    🚚 검색 결과페이지 추가

--- src/AppRoutes.jsx
+++ src/AppRoutes.jsx
@@ -1,13 +1,14 @@
-import React from "react";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from "./pages/Home";
-import Login from "./pages/Login";
-import SignUp from "./pages/SignUp";
-import MovieList from "./pages/MovieList";
-import ReviewList from "./pages/ReviewList";
-import Header from "./components/Header";
-import Footer from "./components/Footer";
+import Home from './pages/Home';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import MovieList from './pages/MovieList';
+// import ReviewList from "./pages/ReviewList";
+import Header from './components/Header';
+import Footer from './components/Footer';
+import SearchResult from './pages/SearchResult';
 
 export default function AppRoutes() {
   return (
@@ -18,9 +19,11 @@ export default function AppRoutes() {
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/movielist" element={<MovieList />} />
-        <Route path="/reviewlist" element={<ReviewList />} />
+        {/* <Route path="/reviewlist" element={<ReviewList />} /> */}
+        <Route path="/searchresult" element={<SearchResult />} />
       </Routes>
-      <Footer />
+
+      {/* <Footer /> */}
     </BrowserRouter>
   );
 }
