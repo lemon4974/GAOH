801c66e329c9f2893f4eff2758eed3c81d9dbeb4
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-20 13:41:44 +0900

    💄 ticket button CSS

--- src/components/Main/Ticket.jsx
+++ src/components/Main/Ticket.jsx
@@ -5,30 +5,6 @@ import '../../styles/home/ticket.scss';
 export default function Ticket() {
   return (
     <div className="ticket-div">
-      {/* <div className="ticket">
-        <div className="ticket-edge-top-left"></div>
-        <div className="ticket-edge-top-right"></div>
-        <div className="ticket-edge-bottom-left"></div>
-        <div className="ticket-edge-bottom-right"></div>
-        <div className="ticket-punches"></div>
-        <div className="ticket-punches-right"></div>
-        <div className="ticket-inner">
-          <div className="ticket-star">
-            <i className="fa fa-star-o"></i>
-          </div>
-          <div className="ticket-headline">SEARCH</div>
-          <div className="ticket-admit">
-            <span className="char">M</span>
-            <span className="char">O</span>
-            <span className="char">V</span>
-            <span className="char">I</span>
-            <span className="char">E</span>
-            <span className="char">S</span>
-          </div>
-        
-        </div>
-      </div> */}
-
       <div className="ticket">Search for Movies</div>
     </div>
   );
