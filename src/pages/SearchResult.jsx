e3ac9c86fecb7940b12d720c6250bb8f3d29862b
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-24 22:40:20 +0900

    💄 검색페이지 검색 input 추가

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,13 +1,13 @@
-import React from 'react';
-import { useLocation } from 'react-router-dom';
+import React from "react";
+import { useLocation } from "react-router-dom";
 
-import '../styles/searchresult/searchresult.scss';
-import MovieResult from '../components/SearchResult/MovieResult';
+import "../styles/searchresult/searchresult.scss";
+import MovieResult from "../components/SearchResult/MovieResult";
 
 export default function SearchResult() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
-  const query = searchParams.get('q');
+  const query = searchParams.get("q");
 
   return (
     <div>
@@ -15,6 +15,20 @@ export default function SearchResult() {
         <div className="search-result-title space-between">
           <div className="title-1">Search Result for "{query}"</div>
           <div className="search-result-title">
+            <div class="search-container">
+              <form action="/search" method="get">
+                <input
+                  class="search expandright"
+                  id="searchright"
+                  type="search"
+                  name="q"
+                  placeholder="Search"
+                />
+                <label class="button searchbutton" for="searchright">
+                  <span class="mglass">&#9906;</span>
+                </label>
+              </form>
+            </div>
             <div className="title-2">latest</div>
             <div className="title-2">popular</div>
           </div>
