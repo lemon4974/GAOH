b5d15cefd213005ad0c4f51a0a243c8d5735073b
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-28 09:04:03 +0900

    🔥 이미지 캐러셀 참고 code 제거

--- src/components/MovieDetail/SwipeableMobileStepper.jsx
+++ src/components/MovieDetail/SwipeableMobileStepper.jsx
@@ -8,32 +8,6 @@ import MobileStepper from "@mui/material/MobileStepper";
 import KeyboardArrowLeft from "@mui/icons-material/KeyboardArrowLeft";
 import KeyboardArrowRight from "@mui/icons-material/KeyboardArrowRight";
 import SwipeableViews from "react-swipeable-views";
-// import { autoPlay } from "react-swipeable-views-utils";
-
-// const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
-
-// const images = [
-//   {
-//     label: "San Francisco – Oakland Bay Bridge, United States",
-//     imgPath:
-//       "https://images.unsplash.com/photo-1537944434965-cf4679d1a598?auto=format&fit=crop&w=400&h=250&q=60",
-//   },
-//   {
-//     label: "Bird",
-//     imgPath:
-//       "https://images.unsplash.com/photo-1538032746644-0212e812a9e7?auto=format&fit=crop&w=400&h=250&q=60",
-//   },
-//   {
-//     label: "Bali, Indonesia",
-//     imgPath:
-//       "https://images.unsplash.com/photo-1537996194471-e657df975ab4?auto=format&fit=crop&w=400&h=250",
-//   },
-//   {
-//     label: "Goč, Serbia",
-//     imgPath:
-//       "https://images.unsplash.com/photo-1512341689857-198e7e2f3ca8?auto=format&fit=crop&w=400&h=250&q=60",
-//   },
-// ];
 
 const itemsPerView = 2; // Number of items per view
 
@@ -47,50 +21,49 @@ export default function SwipeableMobileStepper({ movieId }) {
   const [error, setError] = useState(null);
 
   const handleNext = () => {
-      setActiveStep((prevActiveStep) => prevActiveStep + 1);
-    };
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
 
   const handleBack = () => {
     setActiveStep((prevActiveStep) => prevActiveStep - 1);
   };
-  
+
   const handleStepChange = (step) => {
-      setActiveStep(step);
-    };
-    
-    useEffect(() => {
-        const fetchImages = async () => {
-            // console.log("movieId", movieId);
-            try {
-                const response = await axios.get(
-                    // `https://api.themoviedb.org/3/movie/${movieId}?language=en-US`,
-                    `https://api.themoviedb.org/3/movie/${movieId}/images`,
-                    {
-                        headers: {
-                            accept: "application/json",
-                            Authorization:
-                            "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTgyNDhlY2NiZDUzNGNkYjAxNWY0MDhkNWMyMGUzOCIsInN1YiI6IjY1NjljM2ZmZWEzN2UwMDE0ZWQ2ZWI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0cMyb46qJgcy9qYvXQCKqRfAW9yldC3HPy4YZizCVaM",
-                        },
-                    }
-                    );
-        setImages(response.data.backdrops); // Assuming you want to use the backdrops
+    setActiveStep(step);
+  };
+
+  useEffect(() => {
+    const fetchImages = async () => {
+      // console.log("movieId", movieId);
+      try {
+        const response = await axios.get(
+          `https://api.themoviedb.org/3/movie/${movieId}/images`,
+          {
+            headers: {
+              accept: "application/json",
+              Authorization:
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTgyNDhlY2NiZDUzNGNkYjAxNWY0MDhkNWMyMGUzOCIsInN1YiI6IjY1NjljM2ZmZWEzN2UwMDE0ZWQ2ZWI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0cMyb46qJgcy9qYvXQCKqRfAW9yldC3HPy4YZizCVaM",
+            },
+          }
+        );
+        setImages(response.data.backdrops);
         setLoading(false);
-    } catch (error) {
+      } catch (error) {
         console.error("Error fetching data:", error);
         setError(error);
-    } finally {
+      } finally {
         setLoading(false);
-    }
-};
+      }
+    };
 
-fetchImages();
-}, [movieId]);
+    fetchImages();
+  }, [movieId]);
 
-console.log(images);
-if (loading) return <div>Loading...</div>;
-if (error) return <div>Error: {error.message}</div>;
+  console.log(images);
+  if (loading) return <div>Loading...</div>;
+  if (error) return <div>Error: {error.message}</div>;
 
-const maxSteps = Math.ceil(images.length / 2);
+  const maxSteps = Math.ceil(images.length / 2);
   return (
     <div sx={{ maxWidth: 1200, flexGrow: 1 }}>
       <SwipeableViews
@@ -123,12 +96,7 @@ const maxSteps = Math.ceil(images.length / 2);
                     overflow: "hidden",
                     width: "100%",
                   }}
-                  //   src={image.imgPath}
-                  //   alt={image.label}
-
                   src={`https://image.tmdb.org/t/p/original${image.file_path}`}
-                    // src={`https://image.tmdb.org/t/p/w500${movie.backdrop_path}`}
-                    //   
                   alt={`Image ${imgIndex}`}
                 />
               ))}
