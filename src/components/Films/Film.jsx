bc2fa9ccbcccc0f80a127e0fd2646f7cbc53f933
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-29 22:05:48 +0900

    ✨ Infinite Scroll 데이터와 함께 구현

--- src/components/Films/Film.jsx
+++ src/components/Films/Film.jsx
@@ -1,17 +1,23 @@
-import React, { useState, useEffect } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, useEffect, useRef, useCallback } from 'react';
+import { Link } from 'react-router-dom';
 import axios from 'axios';
 
 export default function Film({ year, filter }) {
-  const [data, setData] = useState(null);
+  const [data, setData] = useState({ results: [] });
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
+  const [page, setPage] = useState(1);
+  const loader = useRef(null);
+
+  const defaultMovieImg =
+    // 'https://cdn.pixabay.com/photo/2019/04/24/21/55/cinema-4153289_1280.jpg';
+    'https://cdn.pixabay.com/photo/2017/11/24/10/43/ticket-2974645_1280.jpg';
 
   useEffect(() => {
     const fetchData = async () => {
       try {
         const response = await axios.get(
-          `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=${year}&region=US&sort_by=${filter}`,
+          `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&primary_release_year=${year}&region=US&sort_by=${filter}`,
           {
             headers: {
               accept: 'application/json',
@@ -20,21 +26,44 @@ export default function Film({ year, filter }) {
             },
           }
         );
-        setData(response.data);
+        setData((prevData) => ({
+          ...prevData,
+          results: [...prevData.results, ...response.data.results],
+        }));
+        setLoading(false);
       } catch (error) {
         setError(error);
-      } finally {
-        setLoading(false);
       }
     };
 
     fetchData();
-  }, [year, filter]);
+  }, [year, filter, page]);
 
   console.log('film data >>', data);
+  console.log('film page >>', page);
 
-  if (loading) return <div>Loading...</div>;
-  if (error) return <div>Error: {error.message}</div>;
+  const handleObserver = useCallback((entries) => {
+    const target = entries[0];
+    if (target.isIntersecting) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  }, []);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(handleObserver, {
+      root: null,
+      rootMargin: '20px',
+      threshold: 1.0,
+    });
+    if (loader.current) observer.observe(loader.current);
+
+    return () => {
+      if (loader.current) observer.unobserve(loader.current);
+    };
+  }, [handleObserver]);
+
+  // if (loading) return <div>Loading...</div>;
+  // if (error) return <div>Error: {error.message}</div>;
 
   return (
     <div>
@@ -50,7 +79,13 @@ export default function Film({ year, filter }) {
               <div className="img-wrapper">
                 <Link to={`/films/detail/${movie.id}`}>
                   <img
-                    src={`https://image.tmdb.org/t/p/original${movie.backdrop_path}`}
+                    src={
+                      movie.backdrop_path || movie.poster_path
+                        ? `https://image.tmdb.org/t/p/original${
+                            movie.backdrop_path || movie.poster_path
+                          }`
+                        : defaultMovieImg
+                    }
                     alt={movie.title}
                   />
                 </Link>
@@ -70,6 +105,9 @@ export default function Film({ year, filter }) {
             </div>
           ))}
       </div>
+      {loading && <p>Loading...</p>}
+      {error && <p>Error: {error.message}</p>}
+      <div ref={loader} />
     </div>
   );
 }
--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -11,8 +11,18 @@ function useFetch(query, page) {
       await setLoading(true);
       await setError(false);
       const res = await axios.get(
-        `https://openlibrary.org/search.json?q=${query}&page=${page}`
+        // `https://openlibrary.org/search.json?q=${query}&page=${page}`
+
+        `https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=${page}&primary_release_year=${query}&region=US`,
+        {
+          headers: {
+            accept: 'application/json',
+            Authorization:
+              'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTgyNDhlY2NiZDUzNGNkYjAxNWY0MDhkNWMyMGUzOCIsInN1YiI6IjY1NjljM2ZmZWEzN2UwMDE0ZWQ2ZWI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0cMyb46qJgcy9qYvXQCKqRfAW9yldC3HPy4YZizCVaM',
+          },
+        }
       );
+
       await setList((prev) => [
         ...new Set([...prev, ...res.data.docs.map((d) => d.title)]),
       ]);
@@ -22,6 +32,7 @@ function useFetch(query, page) {
     }
   }, [query, page]);
 
+  console.log(list);
   useEffect(() => {
     sendQuery(query);
   }, [query, sendQuery, page]);
