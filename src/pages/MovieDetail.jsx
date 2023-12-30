8b67757ca983dcb12a7b350aa92376eabf5b86a8
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2023-12-30 19:30:20 +0900

    💄 디테일page 이미지 변경

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,12 +1,12 @@
-import React, { useState, useEffect } from 'react';
-import { useParams, useNavigate } from 'react-router-dom';
-import axios from 'axios';
+import React, { useState, useEffect } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import axios from "axios";
 
-import '../styles/movieDetail/moviedetail.scss';
-import ImageCarousel from '../components/MovieDetail/ImageCarousel';
+import "../styles/movieDetail/moviedetail.scss";
+import ImageCarousel from "../components/MovieDetail/ImageCarousel";
 // import { SwipeableMobileStepper } from "../components/MovieDetail/SwipeableMobileStepper";
-import SwipeableMobileStepper from '../components/MovieDetail/SwipeableMobileStepper';
-import SwipeableRecommendation from '../components/MovieDetail/SwipeableRecommendation';
+import SwipeableMobileStepper from "../components/MovieDetail/SwipeableMobileStepper";
+import SwipeableRecommendation from "../components/MovieDetail/SwipeableRecommendation";
 
 export default function MovieDetail() {
   const [data, setData] = useState(null);
@@ -33,15 +33,15 @@ export default function MovieDetail() {
           // "https://api.themoviedb.org/3/discover/movie?include_adult=false&include_video=false&language=en-US&page=1&primary_release_year=1953&region=US&sort_by=popularity.desc",
           {
             headers: {
-              accept: 'application/json',
+              accept: "application/json",
               Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTgyNDhlY2NiZDUzNGNkYjAxNWY0MDhkNWMyMGUzOCIsInN1YiI6IjY1NjljM2ZmZWEzN2UwMDE0ZWQ2ZWI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0cMyb46qJgcy9qYvXQCKqRfAW9yldC3HPy4YZizCVaM',
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTgyNDhlY2NiZDUzNGNkYjAxNWY0MDhkNWMyMGUzOCIsInN1YiI6IjY1NjljM2ZmZWEzN2UwMDE0ZWQ2ZWI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0cMyb46qJgcy9qYvXQCKqRfAW9yldC3HPy4YZizCVaM",
             },
           }
         );
         setData(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setError(error);
       } finally {
         setLoading(false);
@@ -60,15 +60,15 @@ export default function MovieDetail() {
           `https://api.themoviedb.org/3/movie/${movieId}/credits?language=en-US`,
           {
             headers: {
-              accept: 'application/json',
+              accept: "application/json",
               Authorization:
-                'Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTgyNDhlY2NiZDUzNGNkYjAxNWY0MDhkNWMyMGUzOCIsInN1YiI6IjY1NjljM2ZmZWEzN2UwMDE0ZWQ2ZWI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0cMyb46qJgcy9qYvXQCKqRfAW9yldC3HPy4YZizCVaM',
+                "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJjMTgyNDhlY2NiZDUzNGNkYjAxNWY0MDhkNWMyMGUzOCIsInN1YiI6IjY1NjljM2ZmZWEzN2UwMDE0ZWQ2ZWI3MiIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.0cMyb46qJgcy9qYvXQCKqRfAW9yldC3HPy4YZizCVaM",
             },
           }
         );
         setActors(response.data);
       } catch (error) {
-        console.error('Error fetching data:', error);
+        console.error("Error fetching data:", error);
         setError(error);
       } finally {
         setLoading(false);
@@ -78,12 +78,12 @@ export default function MovieDetail() {
     fetchActors();
   }, [movieId]);
 
-  console.log('movie detail data >>', data);
-  console.log('movie detail actors >>', actors);
+  console.log("movie detail data >>", data);
+  console.log("movie detail actors >>", actors);
 
   const formatYear = (dateString) => {
-    const options = { year: 'numeric' };
-    return new Date(dateString).toLocaleDateString('en-US', options);
+    const options = { year: "numeric" };
+    return new Date(dateString).toLocaleDateString("en-US", options);
   };
 
   return (
@@ -100,9 +100,9 @@ export default function MovieDetail() {
         {/* <p>Movie ID: {movieId}</p> */}
         <div className="img-wrapper">
           <img
-            src="https://i.ytimg.com/vi/Uqc9XutbOIU/maxresdefault.jpg"
+            src={`https://image.tmdb.org/t/p/original${data && data.backdrop_path}`}
             alt="티파니에서 아침을 이미지"
-            style={{ filter: 'grayscale(100%)' }}
+            style={{ filter: "grayscale(100%)" }}
           />
         </div>
         <div className="movie-data-btn-flex">
