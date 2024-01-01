a8a5824749a7e447747beac3b6131a1cfeca3a9d
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2024-01-01 21:45:50 +0900

    ✨ App.jsx 리코일 세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,30 @@
-import React from 'react';
-import AppRoutes from './AppRoutes';
-import { RecoilRoot } from 'recoil';
-import Main from './components/Main';
+import React from "react";
+import AppRoutes from "./AppRoutes";
+import { RecoilRoot, useRecoilValue } from "recoil";
+
+import { ThemeProvider } from "./ThemeContext";
+import { modeState } from "./state/theme";
+// import Main from './components/Main';
 // import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
+  // const theme = useRecoilValue(modeState);
+
   return (
     <div className="App">
       <RecoilRoot>
-        <AppRoutes />
+        <ThemeProviderComponent>
+          <AppRoutes />
+        </ThemeProviderComponent>
       </RecoilRoot>
     </div>
   );
+}
+
+function ThemeProviderComponent({ children }) {
+  const theme = useRecoilValue(modeState);
+
+  return <ThemeProvider theme={theme}>{children}</ThemeProvider>;
 }
 
 export default App;
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,31 @@
-import React from 'react';
-import { useRecoilState, useRecoilValue } from 'recoil';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import { useRecoilState, useRecoilValue } from "recoil";
+import { Link, useNavigate } from "react-router-dom";
 
 // import { loginState } from '../';
-import { loginState } from '../state/userNameState';
+import { loginState } from "../state/userNameState";
 
-import '../styles/header.scss';
+import "../styles/header.scss";
+import { OriginalState, SnowState, modeState } from "../state/theme";
 
 export default function Header() {
   // const user = useRecoil
   // const loginInfo = useRecoilState(loginState);
-  const [loginInfo, setLoginInfo] = useRecoilState(loginState);
+  // const [loginInfo, setLoginInfo] = useRecoilState(loginState);
+
+  const [divName, setDivName] = useState("snow");
+  const [Theme, setTheme] = useRecoilState(modeState);
+
+  const originalMode = useRecoilValue(OriginalState);
+  const snowMode = useRecoilValue(SnowState);
+
+  const toggle = () => {
+    Theme === originalMode ? setTheme(snowMode) : setTheme(originalMode);
+  };
+
+  const current = useRecoilValue(modeState);
+  // const 
+
   return (
     <header className="wrapper">
       <div className="header-div">
@@ -31,9 +46,9 @@ export default function Header() {
           <Link to="search">
             <div className="link">SEARCH</div>
           </Link>
-          <Link to="login">
-            <div className="link">LOGIN</div>
-          </Link>
+          {/* <Link to="login"> */}
+          <div className="link" onClick={toggle}>Theme</div>
+          {/* </Link> */}
         </div>
 
         {/* 로고// 메인,검색  로그인or로그아웃 */}
