557fb35049a996b566fd66f9f66b9dac607644a6
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2024-01-03 20:40:17 +0900

    💄 404 페이지 CSS

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,9 +1,32 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+
+import '../styles/errorPg/error.scss';
 
 export default function Error() {
   return (
     <div className="wrapper">
-      <div>404</div>
+      <div className="flex-container">
+        <div className="text-center">
+          <h1>
+            <span className="fade-in" id="digit1">
+              4
+            </span>
+            <span className="fade-in" id="digit2">
+              0
+            </span>
+            <span className="fade-in" id="digit3">
+              4
+            </span>
+          </h1>
+          <h3 className="fadeIn">PAGE NOT FOUND</h3>
+          <Link to="/" className="Icon">
+            <button type="button" name="button">
+              Return To Home
+            </button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 }
