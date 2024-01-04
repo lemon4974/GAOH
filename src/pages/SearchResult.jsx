24bf086797f67fd4675f619808c4450bc40929e6
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2024-01-04 16:31:10 +0900

    💄 Search bar 최적화 및 반응형

--- src/pages/SearchResult.jsx
+++ src/pages/SearchResult.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { useLocation } from 'react-router-dom';
 
 import '../styles/searchresult/searchresult.scss';
 import MovieResult from '../components/SearchResult/MovieResult';
 
+import ClearRoundedIcon from '@mui/icons-material/ClearRounded';
+
 export default function SearchResult() {
   const location = useLocation();
   const searchParams = new URLSearchParams(location.search);
   const query = searchParams.get('q');
+
+  const [searchValue, setSearchValue] = useState('');
+
+  const handleClear = () => {
+    setSearchValue(''); // Clear the search input
+  };
 
   return (
     <div>
@@ -27,7 +35,20 @@ export default function SearchResult() {
                   type="search"
                   name="q"
                   placeholder="Search"
+                  value={searchValue}
+                  onChange={(e) => setSearchValue(e.target.value)}
                 />
+                {searchValue && (
+                  <ClearRoundedIcon
+                    style={{
+                      color: '#eb4d33',
+                      cursor: 'pointer',
+                      position: 'absolute',
+                      right: '30px',
+                    }}
+                    onClick={handleClear}
+                  />
+                )}
                 <label className="button searchbutton" htmlFor="searchright">
                   <span className="mglass">&#9906;</span>
                 </label>
