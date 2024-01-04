d29886fcaa21aef0d548b4baf88bfab901e209e1
lemon4974 <139740955+lemon4974@users.noreply.github.com>
2024-01-04 17:59:01 +0900

    🚑 필름 페이지: Infinite Scroll 기능 & return 로딩 삭제 해결

--- src/components/Films/Film.jsx
+++ src/components/Films/Film.jsx
@@ -103,12 +103,12 @@ export default function Film({ year, filter }) {
     };
   }, [handleObserver]);
 
-  if (loading)
-    return (
-      <div>
-        <Loading />
-      </div>
-    );
+  // if (loading)
+  //   return (
+  //     <div>
+  //       <Loading />
+  //     </div>
+  //   );
   if (error) return <div>Error: {error.message}</div>;
 
   return (
